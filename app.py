41e0b87eae8514a28d6193288f210d0609459604
Sardor Hasanov <154466359+sardorbekhasan@users.noreply.github.com>
2024-09-13 15:31:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import platform
 
 plt = platform.system()
-if plt == 'Linux: pathlib.WindowsPath = pathlib.PosixPath
+if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
 st.title('Transportni klassifikatsiya qiluvchi model')
 
