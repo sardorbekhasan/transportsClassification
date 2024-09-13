bf6852ae90d51606ec2bac5b42f1ede3a76c261d
Sardor Hasanov <154466359+sardorbekhasan@users.noreply.github.com>
2024-09-13 15:35:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ st.title('Transportni klassifikatsiya qiluvchi model')
 
 file = st.file_uploader('Rasm yuklash', type=['png','jpeg', 'gif', 'svg'])
 if file:
-    img = PILImage.create(file)
     st.image(file)
     
-    model = load_learner('transport_model.pkl')
-    pred, pred_id, probs = model.predict(img)
+    img = PILImage.create(file)
     
+    model = load_learner('transport_model.pkl')
+    
+    pred, pred_id, probs = model.predict(img)
     st.success(f"Bashorat:{pred}")
     st.info(f"Ehtimollik:{probs[pred_id]*100:.1f}%")
     fig = px.bar(y=model.dls.vocab, x=probs*100)    
